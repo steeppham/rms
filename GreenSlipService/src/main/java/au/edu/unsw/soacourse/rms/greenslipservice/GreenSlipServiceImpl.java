4290ecdade21d5c50e54adc9078b3c2018e0e5f7
Stephen Pham <pham.stephen@gmail.com>
2015-05-23 11:30:20 +1000

    Fix GreenSlipService to return result if driver is located.

--- GreenSlipService/src/main/java/au/edu/unsw/soacourse/rms/greenslipservice/GreenSlipServiceImpl.java
+++ GreenSlipService/src/main/java/au/edu/unsw/soacourse/rms/greenslipservice/GreenSlipServiceImpl.java
@@ -21,43 +21,15 @@ public class GreenSlipServiceImpl implements GreenSlipService {
 	
 	private final String XML_DATABASE = "GreenSlip_Database.xml";
 	
+	private Document doc;
+	
 	@Override
 	public GSCheckResponse gsCheck(GSCheckRequest parameters)
 			throws DataAccessFaultException, DriverNotFoundFaultException {
-		
-		// Read XML file into memory
-		InputStream file = getClass().getClassLoader().getResourceAsStream("/" + XML_DATABASE);
-		if (file == null) {
-			throw new DataAccessFaultException("Cannot find " + XML_DATABASE);
-		}
-		
-		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-		DocumentBuilder dBuilder;
-		Document doc;
-		try {
-			dBuilder = dbFactory.newDocumentBuilder();
-			doc = dBuilder.parse(file);	
-		} catch (ParserConfigurationException | SAXException | IOException e) {
-			e.printStackTrace();
-			throw new DataAccessFaultException("Failed to read " + XML_DATABASE, e);
-		}
-		
-		// Find the driver
-		XPath xPath =  XPathFactory.newInstance().newXPath();
-		String expression = String.format("Customers/Entry/RegistrationNumber[text()='%s']/../CTPDetails/CurrentlyValid/text()", parameters.getRegoNumber());
-		String valid = null;
-		try {
-			valid = xPath.compile(expression).evaluate(doc);
-		} catch (XPathExpressionException e) {
-			e.printStackTrace();
-			throw new DataAccessFaultException("Fail to search for " + XML_DATABASE, e);
-		}
-		
-		if (valid == null) {
-			throw new DriverNotFoundFaultException("Cannot locate driver");
-		}
-
-		boolean paid = valid.equalsIgnoreCase("yes") ? true : false;
+		String expression = String.format("Customers/Entry/Driver/LastName[text()='%s']/../FirstName[text()='%s']/../../RegistrationNumber[text()='%s']/../CTPDetails/CurrentlyValid/text()", 
+				parameters.lastname, parameters.firstname, parameters.getRegoNumber());
+		String currentlyValid = readDatabase(expression);
+		boolean paid = currentlyValid.equalsIgnoreCase("yes") ? true : false;
 		
 		GSCheckResponse response = factory.createGSCheckResponse();
 		response.setFirstname(parameters.getFirstname());
@@ -66,5 +38,50 @@ public class GreenSlipServiceImpl implements GreenSlipService {
 		response.setPaidFlag(paid);
 		return response;
 	}
+	
+	private String readDatabase(String query) throws DataAccessFaultException, DriverNotFoundFaultException {
+		// Read XML file into memory
+		if (doc == null) {
+			InputStream file = getClass().getClassLoader().getResourceAsStream(
+					"/" + XML_DATABASE);
+			if (file == null) {
+				throw new DataAccessFaultException("Cannot find " + XML_DATABASE);
+			}
+	
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dBuilder;
+			try {
+				dBuilder = dbFactory.newDocumentBuilder();
+				doc = dBuilder.parse(file);
+			} catch (ParserConfigurationException | SAXException | IOException e) {
+				e.printStackTrace();
+				throw new DataAccessFaultException(
+						"Failed to read " + XML_DATABASE, e);
+			}
+			try {
+				file.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+				throw new DataAccessFaultException(
+						"Failed to close " + XML_DATABASE, e);
+			}
+		}
 
+		// Find the driver
+		XPath xPath = XPathFactory.newInstance().newXPath();
+		String result = null;
+		try {
+			result = xPath.compile(query).evaluate(doc);
+		} catch (XPathExpressionException e) {
+			e.printStackTrace();
+			throw new DataAccessFaultException("Fail to search for "
+					+ XML_DATABASE, e);
+		}
+
+		if (result.isEmpty()) {
+			throw new DriverNotFoundFaultException("Cannot locate driver");
+		}
+		
+		return result;
+	}
 }
